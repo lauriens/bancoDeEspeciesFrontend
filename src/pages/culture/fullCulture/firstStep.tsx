492f9a75aa4b3a4cd019d2e146769b2b3a8f781a
Laurien <laurien.santin@poatek.com>
2024-02-14 19:44:03 -0300

    added collect methods to full culture first step

--- src/pages/culture/fullCulture/firstStep.tsx
+++ src/pages/culture/fullCulture/firstStep.tsx
@@ -4,6 +4,7 @@ import { Collapse } from 'antd'
 import AnthromePage from '../../landscape/anthrome/anthrome'
 import AgroecosystemPage from '../../landscape/agroecosystem/agroecosystem'
 import SampleAreaTypePage from '../../landscape/sampleAreaType/sampleAreaType'
+import CollectMethodPage from '../../occurrence/collectMethod/collectMethod'
 
 function FirstStep({ visible }: StepProps) {
     if (!visible) return null
@@ -26,6 +27,12 @@ function FirstStep({ visible }: StepProps) {
                 defaultActiveKey={undefined}
                 items={[
                     { key: '1', label: 'Tipo de Área Amostrada', children: <SampleAreaTypePage /> }
+                ]}
+            />
+            <Collapse
+                defaultActiveKey={undefined}
+                items={[
+                    { key: '1', label: 'Métodos de Coleta', children: <CollectMethodPage /> }
                 ]}
             />
         </div>
